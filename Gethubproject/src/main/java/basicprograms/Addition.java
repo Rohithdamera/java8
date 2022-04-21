c0344ea8bfef1cf21922e4ff6cbec80b93eca8cf
rohith <myselfrohith98@gmail.com>
2022-04-21 15:59:50 -0700

    basic program

---
+++ Gethubproject/src/main/java/basicprograms/Addition.java
@@ -0,0 +1,15 @@
+package basicprograms;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		int j=2;
+		int k=i+j;
+		System.out.println(k);
+		
+
+	}
+
+}
